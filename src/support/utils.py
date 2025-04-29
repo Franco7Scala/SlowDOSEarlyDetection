7070d1e6f7e26533d9e8c9be7dba8ad383bd8a00
Francesco Scala <franco7scala@gmail.com>
2025-04-29 17:06:51 +0200

    added seed everything method

--- src/support/utils.py
+++ src/support/utils.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 import torch
+import random
+import os
+
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.utils import compute_class_weight
 
@@ -84,3 +87,13 @@ def splitDataset(data: torch.Tensor, target: torch.Tensor, size1: float, size2: 
         y_train, y_test = target[train_index], target[test_index]
     return x_train, x_test, y_train, y_test
 #-----datasets utils-----#
+
+def seed_everything(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    
